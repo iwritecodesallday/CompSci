3339a78c5ce4a9249c7621f40bef1254ecd65535
Steven <steven.charles@zealicsolutions.com>
2019-08-26 19:03:50 -0400

    Update Chapter 1 and 2

---
+++ Chapter 1/2.2/math.cs
@@ -0,0 +1,11 @@
+using System;
+
+class math {
+    public static void Main(string[] args)
+        {
+            int cos;
+            cos = args[0];
+            Math.Sin(cos);
+            Console.WriteLine(cos.ToString);
+        }
+}
