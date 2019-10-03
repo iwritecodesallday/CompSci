e9148b0cd75f2e3a0cff49635304ecacc4194b56
Vashchenko Vladyslav <vashchenko.vladyslav@zealicsolutions.com>
2019-10-03 18:21:16 +0300

    4.4

--- Chapter 1/4/4.1/example.cs
+++ Chapter 1/4/4.1/example.cs
@@ -1,5 +1,8 @@
 using System;
 
+// Write a program that declares, creates, and initializes an array a[] of length 1000 and accesses a[1000]. 
+// Does your program compile? What happens when you run it?
+
 public class example {
     public static void Main(string[] args) {
         double[] a;
--- Chapter 1/4/4.4/example.cs
+++ Chapter 1/4/4.4/example.cs
@@ -1,9 +1,25 @@
 using System;
 
-public class example {
-    public static void Main(string[] args) {
-        int[] a;
-        for (int i = 0; i < 10; i++)
-            a[i] = i * i;
+// Write a code fragment that reverses the order of the values in a one
+// dimensional array. Do not create another array to hold the result.
+
+public class example
+{
+    public static void Main(string[] args)
+    {
+
+        int[] arr = { 1, 3, 4, 9, 8 };
+        for (int i = 0; i < arr.Length / 2; i++)
+        {
+            {
+                int tmp = arr[i];
+                arr[i] = arr[arr.Length - i - 1];
+                arr[arr.Length - i - 1] = tmp;
+            }
+            Console.WriteLine(string.Join(", ", arr));
+        }
+
+
+
     }
 }
