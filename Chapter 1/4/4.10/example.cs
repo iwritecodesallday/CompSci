c0de968ba505c107c8245b99ec45982f52274712
Vashchenko Vladyslav <vashchenko.vladyslav@zealicsolutions.com>
2019-10-07 18:05:15 +0300

    Updated Chapter

--- Chapter 1/4/4.10/example.cs
+++ Chapter 1/4/4.10/example.cs
@@ -4,80 +4,48 @@ public class deal
 {
     public static void Main(string[] args)
     {
-        Random ran = new Random();
-        // All Suits
-        String[] Clubs = {
-            "2 of Clubs",
-            "3 of Clubs",
-            "4 of Clubs",
-            "5 of Clubs",
-            "6 of Clubs",
-            "7 of Clubs",
-            "8 of Clubs",
-            "9 of Clubs",
-            "10 of Clubs",
-            "Jack of Clubs",
-            "Queen of Clubs",
-            "King of Clubs",
-            "Ace of Clubs"
-        };
-        String[] Spades = {
-            "2 of Spades",
-            "3 of Spades",
-            "4 of Spades",
-            "5 of Spades",
-            "6 of Spades",
-            "7 of Spades",
-            "8 of Spades",
-            "9 of Spades",
-            "10 of Spades",
-            "Jack of Spades",
-            "Queen of Spades",
-            "King of Spades",
-            "Ace of Spades"
-        };
-        String[] Diamonds = {
-            "2 of Diamonds",
-            "3 of Diamonds",
-            "4 of Diamonds",
-            "5 of Diamonds",
-            "6 of Diamonds",
-            "7 of Diamonds",
-            "8 of Diamonds",
-            "9 of Diamonds",
-            "10 of Diamonds",
-            "Jack of Diamonds",
-            "Queen of Diamonds",
-            "King of Diamonds",
-            "Ace of Diamonds"
-        };
-        String[] Hearts = {
-            "2 of Hearts",
-            "3 of Hearts",
-            "4 of Hearts",
-            "5 of Hearts",
-            "6 of Hearts",
-            "7 of Hearts",
-            "8 of Hearts",
-            "9 of Hearts",
-            "10 of Hearts",
-            "Jack of Hearts",
-            "Queen of Hearts",
-            "King of Hearts",
-            "Ace of Hearts"
-        };
+        // Deck Modal
+        String[] SUITS = { "Clubs", "Diamonds", "Hearts", "Spades" };
+        String[] RANKS = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+        String[] deck = new String[RANKS.Length * SUITS.Length];
+        for (int i = 0; i < RANKS.Length; i++)
+            for (int j = 0; j < SUITS.Length; j++)
+                deck[SUITS.Length * i + j] = RANKS[i] + " of " + SUITS[j];
 
-        string[] cards = new String[5];
+        // m = cards in hand
+        // n = cards in deck
+        Int32 m = 5;
+        Int32 n = 52;
 
-        for (int a = 0; a < 5; a++)
+        Int32[] perm = new int[n];
+        Random random = new Random();
+
+        // Without this for loop, card is the same and is drawn 5 times
+        for (int j = 0; j < n; j++)
         {
-            int i = (int)(ran.Next(1, RANKS.Length));
-            int j = (int)(ran.Next(1, SUITS.Length));
-            cards[a] = (RANKS[i] + " of " + SUITS[j]);
-            Console.WriteLine(cards);
+            perm[j] = j;
         }
-        
+
+        // Without this loop, cards are drawn in order
+        for (int i = 0; i < m; i++)
+        {
+            int r = i + (int)(random.Next(n - i));
+            int t = perm[r];
+            perm[r] = perm[i];
+            perm[i] = t;
+        }
+
+        // Without this loop, cards are not drawn
+        for (int i = 0; i < m; i++)
+        {
+            Console.WriteLine(deck[perm[i]]);
+        }
+        Console.WriteLine();
+
+
     }
+
+
     // Question:
     // Write a program "Deal" that takes an integer command-line argument "n" and prints "n poker hands" 
     // (five cards each) from a shuffled deck, seperated by blank lines
